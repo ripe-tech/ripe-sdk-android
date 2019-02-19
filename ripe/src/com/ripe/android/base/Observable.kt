021cc2569c571a55d57a2ee3bb5288dd280db8a2
Afonso Caldas <anc@ripe.tech>
2019-02-19 14:55:13 +0000

    initial callable

--- ripe/src/com/ripe/android/base/Observable.kt
+++ ripe/src/com/ripe/android/base/Observable.kt
@@ -1,16 +1,18 @@
 package com.ripe.android.base
 
-open class Observable {
-    val callbacks: MutableMap<String, MutableList<(args: Map<String, Any>) -> Unit>> = HashMap()
+import kotlin.reflect.KFunction
 
-    fun addCallback(event: String, callback: (args: Map<String, Any>) -> Unit): (args: Map<String, Any>) -> Unit {
+open class Observable {
+    val callbacks: MutableMap<String, MutableList<KFunction<Unit>>> = HashMap()
+
+    fun addCallback(event: String, callback: KFunction<Unit>): KFunction<Unit> {
         val callbacks = this.callbacks[event] ?: ArrayList()
         callbacks.add(callback)
         this.callbacks[event] = callbacks
         return callback
     }
 
-    fun removeCallback(event: String, callback: ((args: Map<String, Any>) -> Unit)?) {
+    fun removeCallback(event: String, callback: (KFunction<Unit>)?) {
         val callbacks = this.callbacks[event] ?: ArrayList()
         if (callback != null) {
             callbacks.remove(callback)
@@ -22,10 +24,10 @@ open class Observable {
 
     fun runCallbacks(event: String, args: Map<String, Any> = HashMap()) {
         val callbacks = this.callbacks[event] ?: ArrayList()
-        callbacks.forEach { it(args) }
+        callbacks.forEach { it.call(args) }
     }
 
-    fun bind(event: String, callback: (args: Map<String, Any>) -> Unit) = addCallback(event, callback)
-    fun unbind(event: String, callback: ((args: Map<String, Any>) -> Unit)?) = removeCallback(event, callback)
+    fun bind(event: String, callback: KFunction<Unit>) = addCallback(event, callback)
+    fun unbind(event: String, callback: (KFunction<Unit>)?) = removeCallback(event, callback)
     fun trigger(event: String, args: Map<String, Any> = HashMap()) = runCallbacks(event, args)
 }
--- ripe/src/com/ripe/android/test/ObservableTest.kt
+++ ripe/src/com/ripe/android/test/ObservableTest.kt
@@ -5,16 +5,15 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 
 import com.ripe.android.base.Observable
+import kotlin.reflect.KFunction
 
 class ObservableTest {
 
     @Test
     fun testAddRemove() {
         val observable = Observable()
-        val callback = observable.addCallback("test") {
-            val value = it as Map<String, String>
-            print("${value["a"]} ${value["b"]}")
-        }
+        observable.addCallback("test", callback = KFunction)
+        val callback = observable.addCallback("test") {}
         assertNotNull(observable.callbacks["test"])
         assertEquals(observable.callbacks["test"]!!.size, 1)
 
